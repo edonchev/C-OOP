de7fdcb0979be85cf06f0ac3c977efde8d1531ae
EnchoDonchev <32332495+edonchev@users.noreply.github.com>
2019-05-07 15:53:14 +0300

    New Solution

---
+++ Polymorphism/Vehicles/Truck.cs
@@ -0,0 +1,17 @@
+﻿namespace Vehicles
+{
+    internal class Truck : Vehicle
+    {
+        private static readonly double increasedFuelConsumption = 1.6;
+        private static readonly double quantityLoss = 0.95;
+
+        public Truck(double fuelQuantity, double fuelConsumption) : base(fuelQuantity, fuelConsumption + increasedFuelConsumption)
+        {
+        }
+    
+        public override void Refuel(double liters)
+        {
+            FuelQuantity += liters * quantityLoss;
+        }
+    }
+}
---
+++ Polymorphism/Vehicles/VehicleFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Vehicles
+{
+    public static class VehicleFactory
+    {
+        public static Vehicle CreateVehicle()
+        {
+            string[] vehicle = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string vehicleType = vehicle[0];
+            double fuelQuantity = double.Parse(vehicle[1]);
+            double litersPerKm = double.Parse(vehicle[2]);
+
+            switch (vehicleType)
+            {
+                case "Car":
+                    return new Car(fuelQuantity, litersPerKm);
+                case "Truck":
+                    return new Truck(fuelQuantity, litersPerKm);
+                default:
+                    return null;
+            }
+        }
+    }
+}
